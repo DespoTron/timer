c60f0ce2095a89eb1c4a8e2cb511a269368534c3
Thai Do <thai.hl.do@gmail.com>
2020-08-24 00:34:34 +0000

    Refactored with some notes to understand whats going on

--- timer1.js
+++ timer1.js
@@ -1,27 +1,15 @@
 let args = process.argv.slice(2);
 
 const input = function(num) {
+  if (!num) return; // No numbers are provided ends immediately
 
-  if(!num) return
-
-  let goodNums = num.filter(number => number > 0 && number !== NaN);
-  for(let alarm of goodNums) {
+  let goodNums = num.filter(number => number > 0 && number !== isNaN); // use filter method to take only numbers that are greater than 0 and is a Number
+  for (let alarm of goodNums) { // loop through those goodNumbers
     setTimeout(() => {
-      process.stdout.write('\x07');
-      process.stdout.write(`${alarm} seconds\n`)
-    }, alarm * 1000)
+      process.stdout.write('\x07'); // emit a beep
+      process.stdout.write(`${alarm} seconds\n`); // console log out the number and seconds
+    }, alarm * 1000); // number * 1 second
   }
 };
 
 input(args);
-
-// filter takes an array and function as argumentfunction 
-//filter(arr, filterFunc) {    
-  //const filterArr = []; // empty array        
-  // loop though array    
-  //for(let i=0;i<arr.length;i++) {        
-    //const result = filterFunc(arr[i], i, arr);        
-    // push the current element if result is true        
-    //if(result)             
-    //filterArr.push(arr[i]);     }    
-    //return filterArr;}
